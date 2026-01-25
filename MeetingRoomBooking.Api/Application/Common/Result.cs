5d08dbb342ed034b0ee75714bf4bbd2dcb9f3c5c
Eeroojanen <eeroojanen0@gmail.com>
2026-01-25 15:44:05 +0200

    korjaus: Lisätty null-turvallisuus Result<T>-tyyppiin

--- MeetingRoomBooking.Api/Application/Common/Result.cs
+++ MeetingRoomBooking.Api/Application/Common/Result.cs
@@ -4,13 +4,34 @@ namespace MeetingRoomBooking.Api.Application.Common;
 public readonly record struct Unit;
 
 // Lightweight result union
-public sealed record Result<T>(T? Value, ApiError? Error)
+public sealed record Result<T>(T? Value, ApiError? Error) where T : notnull
 {
     public bool IsSuccess => Error is null;
 
     public TResult Match<TResult>(Func<T, TResult> onSuccess, Func<ApiError, TResult> onError)
-        => IsSuccess ? onSuccess(Value!) : onError(Error!);
+    {
+        if (!IsSuccess)
+        {
+            return onError(Error!);
+        }
 
-    public static Result<T> Success(T value) => new(value, null);
+        if (Value is null)
+        {
+            throw new InvalidOperationException("Result was successful but Value was null.");
+        }
+
+        return onSuccess(Value);
+    }
+
+    public static Result<T> Success(T value)
+    {
+        if (value is null)
+        {
+            throw new ArgumentNullException(nameof(value), "Success value cannot be null.");
+        }
+
+        return new(value, null);
+    }
+
     public static Result<T> Fail(ApiError error) => new(default, error);
 }
